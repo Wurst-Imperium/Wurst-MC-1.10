e9f838506696571043515634c22edbf122d15666
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-21 19:51:59 +0200

    Change version to 4.1

--- Wurst Client for MC 1.10.X/src/tk/wurst_client/WurstClient.java
+++ Wurst Client for MC 1.10.X/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
