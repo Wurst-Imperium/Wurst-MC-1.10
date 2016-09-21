32d7143989aec29d8dda1c461641a466744cc073
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-21 20:56:00 +0200

    Improve FightBot
    Resolves Wurst-Imperium/Wurst-Client-Bugs#156

--- Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/FightBotMod.java
+++ Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/FightBotMod.java
@@ -13,6 +13,11 @@ import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Bypasses;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
+import tk.wurst_client.navigator.NavigatorItem;
+import tk.wurst_client.navigator.settings.CheckboxSetting;
+import tk.wurst_client.navigator.settings.SliderSetting;
+import tk.wurst_client.navigator.settings.SliderSetting.ValueDisplay;
+import tk.wurst_client.special.YesCheatSpf.BypassLevel;
 import tk.wurst_client.utils.EntityUtils;
 
 @Info(category = Category.COMBAT,
@@ -24,9 +29,58 @@ import tk.wurst_client.utils.EntityUtils;
 @Bypasses(ghostMode = false)
 public class FightBotMod extends Mod implements UpdateListener
 {
-	private float range = 6F;
-	private double distance = 3D;
-	private EntityLivingBase entity;
+	public CheckboxSetting useKillaura =
+		new CheckboxSetting("Use Killaura settings", true)
+		{
+			@Override
+			public void update()
+			{
+				if(isChecked())
+				{
+					KillauraMod killaura = wurst.mods.killauraMod;
+					useCooldown.lock(killaura.useCooldown.isChecked());
+					speed.lockToValue(killaura.speed.getValue());
+					range.lockToValue(killaura.range.getValue());
+				}else
+				{
+					useCooldown.unlock();
+					speed.unlock();
+					range.unlock();
+				}
+			};
+		};
+	public CheckboxSetting useCooldown =
+		new CheckboxSetting("Use Attack Cooldown as Speed", true)
+		{
+			@Override
+			public void update()
+			{
+				speed.setDisabled(isChecked());
+			};
+		};
+	public SliderSetting speed =
+		new SliderSetting("Speed", 20, 2, 20, 0.1, ValueDisplay.DECIMAL);
+	public SliderSetting range =
+		new SliderSetting("Range", 6, 1, 6, 0.05, ValueDisplay.DECIMAL);
+	public SliderSetting distance =
+		new SliderSetting("Distance", 3, 1, 6, 0.05, ValueDisplay.DECIMAL);
+	
+	@Override
+	public void initSettings()
+	{
+		settings.add(useKillaura);
+		settings.add(useCooldown);
+		settings.add(speed);
+		settings.add(range);
+		settings.add(distance);
+	}
+	
+	@Override
+	public NavigatorItem[] getSeeAlso()
+	{
+		return new NavigatorItem[]{wurst.mods.killauraMod,
+			wurst.special.targetSpf, wurst.special.yesCheatSpf};
+	}
 	
 	@Override
 	public void onEnable()
@@ -37,45 +91,50 @@ public class FightBotMod extends Mod implements UpdateListener
 	@Override
 	public void onUpdate()
 	{
-		entity = EntityUtils.getClosestEntity(true, 360, false);
-		if(entity == null)
+		// set target
+		EntityLivingBase target =
+			EntityUtils.getClosestEntity(true, 360, false);
+		if(target == null)
 			return;
-		if(entity.getHealth() <= 0 || entity.isDead
-			|| mc.thePlayer.getHealth() <= 0)
-		{
-			entity = null;
-			mc.gameSettings.keyBindForward.pressed = false;
-			return;
-		}
-		double xDist = Math.abs(mc.thePlayer.posX - entity.posX);
-		double zDist = Math.abs(mc.thePlayer.posZ - entity.posZ);
-		EntityUtils.faceEntityClient(entity);
-		if(xDist > distance || zDist > distance)
+		
+		// face target
+		EntityUtils.faceEntityClient(target);
+		
+		// walk to target
+		if(mc.thePlayer.getDistanceToEntity(target) > distance.getValueF())
 			mc.gameSettings.keyBindForward.pressed = true;
 		else
 			mc.gameSettings.keyBindForward.pressed = false;
+		
+		// jump
 		if(mc.thePlayer.isCollidedHorizontally && mc.thePlayer.onGround)
 			mc.thePlayer.jump();
-		if(mc.thePlayer.isInWater() && mc.thePlayer.posY < entity.posY)
+		
+		// swim
+		if(mc.thePlayer.isInWater() && mc.thePlayer.posY < target.posY)
 			mc.thePlayer.motionY += 0.04;
+		
+		// attack target
 		updateMS();
-		if(hasTimePassedS(wurst.mods.killauraMod.speed.getValueF()))
-			if(mc.thePlayer.getDistanceToEntity(entity) <= range)
+		if((useCooldown.isChecked()
+			? mc.thePlayer.getCooledAttackStrength(0F) >= 1F
+			: hasTimePassedS(speed.getValueF()))
+			&& mc.thePlayer.getDistanceToEntity(target) <= range.getValueF())
+		{
+			if(wurst.mods.autoSwordMod.isActive())
+				AutoSwordMod.setSlot();
+			wurst.mods.criticalsMod.doCritical();
+			wurst.mods.blockHitMod.doBlock();
+			if(EntityUtils.getDistanceFromMouse(target) > 55)
+				EntityUtils.faceEntityClient(target);
+			else
 			{
-				if(wurst.mods.autoSwordMod.isActive())
-					AutoSwordMod.setSlot();
-				wurst.mods.criticalsMod.doCritical();
-				wurst.mods.blockHitMod.doBlock();
-				if(EntityUtils.getDistanceFromMouse(entity) > 55)
-					EntityUtils.faceEntityClient(entity);
-				else
-				{
-					EntityUtils.faceEntityClient(entity);
-					mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
-					mc.playerController.attackEntity(mc.thePlayer, entity);
-				}
-				updateLastMS();
+				EntityUtils.faceEntityClient(target);
+				mc.playerController.attackEntity(mc.thePlayer, target);
+				mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
 			}
+			updateLastMS();
+		}
 	}
 	
 	@Override
@@ -83,5 +142,28 @@ public class FightBotMod extends Mod implements UpdateListener
 	{
 		wurst.events.remove(UpdateListener.class, this);
 		mc.gameSettings.keyBindForward.pressed = false;
+	}
+	
+	@Override
+	public void onYesCheatUpdate(BypassLevel bypassLevel)
+	{
+		switch(bypassLevel)
+		{
+			default:
+			case OFF:
+			case MINEPLEX_ANTICHEAT:
+				speed.unlock();
+				range.unlock();
+				distance.unlock();
+				break;
+			case ANTICHEAT:
+			case OLDER_NCP:
+			case LATEST_NCP:
+			case GHOST_MODE:
+				speed.lockToMax(12);
+				range.lockToMax(4.25);
+				distance.lockToMax(4.25);
+				break;
+		}
 	}
 }
