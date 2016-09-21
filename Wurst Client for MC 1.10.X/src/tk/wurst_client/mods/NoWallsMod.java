70034372d8d0f447d6f3dbc460e8b82845977057
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-21 17:57:51 +0200

    :gem: Clean up

--- Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/NoWallsMod.java
+++ Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/NoWallsMod.java
@@ -1,27 +1,19 @@
 /*
- * Copyright Â© 2014 - 2016 | Wurst-Imperium | All rights reserved.
- * 
+ * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.mods;
 
-import java.util.ArrayList;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.world.GameType;
-
 import tk.wurst_client.events.PacketOutputEvent;
-import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.events.listeners.PacketOutputListener;
-
+import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Bypasses;
 import tk.wurst_client.mods.Mod.Category;
-
 import tk.wurst_client.mods.Mod.Info;
 
 @Info(category = Category.MOVEMENT,
@@ -29,15 +21,16 @@ import tk.wurst_client.mods.Mod.Info;
 	name = "NoWalls",
 	help = "Mods/NoWalls")
 @Bypasses
-public class NoWallsMod extends Mod implements PacketOutputListener, UpdateListener
-{	
+public class NoWallsMod extends Mod
+	implements PacketOutputListener, UpdateListener
+{
 	
 	@Override
 	public void onEnable()
 	{
 		if(wurst.mods.noClipMod.isEnabled())
 			wurst.mods.noClipMod.setEnabled(false);
-		wurst.events.add(PacketOutputListener.class, this);		
+		wurst.events.add(PacketOutputListener.class, this);
 		wurst.events.add(UpdateListener.class, this);
 	}
 	
@@ -46,9 +39,7 @@ public class NoWallsMod extends Mod implements PacketOutputListener, UpdateListe
 	{
 		Packet packet = event.getPacket();
 		if(packet instanceof CPacketPlayer)
-		{
 			event.cancel();
-		}
 	}
 	
 	@Override
@@ -70,7 +61,6 @@ public class NoWallsMod extends Mod implements PacketOutputListener, UpdateListe
 		if(mc.gameSettings.keyBindSneak.pressed)
 			mc.thePlayer.motionY -= speed;
 	}
-
 	
 	@Override
 	public void onDisable()
@@ -82,7 +72,7 @@ public class NoWallsMod extends Mod implements PacketOutputListener, UpdateListe
 			mc.thePlayer.posX, mc.thePlayer.getEntityBoundingBox().minY,
 			mc.thePlayer.posZ, mc.thePlayer.cameraYaw, mc.thePlayer.cameraPitch,
 			mc.thePlayer.onGround));
-			
+		
 		mc.thePlayer.noClip = false;
 	}
 	
