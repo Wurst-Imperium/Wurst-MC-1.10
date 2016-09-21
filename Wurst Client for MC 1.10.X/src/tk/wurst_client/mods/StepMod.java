fe8872f584f56d2692ac3a699b03a2dda63345a8
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-21 17:46:30 +0200

    Improve Step
    Resolves #13

--- Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/StepMod.java
+++ Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/StepMod.java
@@ -7,7 +7,12 @@
  */
 package tk.wurst_client.mods;
 
+import java.util.ArrayList;
+
+import net.minecraft.block.BlockFenceGate;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.math.BlockPos;
 import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Bypasses;
 import tk.wurst_client.mods.Mod.Category;
@@ -45,7 +50,11 @@ public class StepMod extends Mod implements UpdateListener
 			.ordinal() >= BypassLevel.ANTICHEAT.ordinal())
 		{
 			mc.thePlayer.stepHeight = 0.5F;
-			if(mc.thePlayer.isCollidedHorizontally && mc.thePlayer.onGround)
+			if(mc.thePlayer.onGround && !mc.thePlayer.isOnLadder()
+				&& (mc.thePlayer.movementInput.moveForward != 0.0F
+					|| mc.thePlayer.movementInput.moveStrafe != 0.0F)
+				&& canStep() && !mc.thePlayer.movementInput.jump
+				&& mc.thePlayer.isCollidedHorizontally)
 			{
 				mc.getConnection()
 					.sendPacket(new CPacketPlayer.Position(mc.thePlayer.posX,
@@ -86,5 +95,41 @@ public class StepMod extends Mod implements UpdateListener
 				height.lockToValue(1);
 				break;
 		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	private boolean canStep()
+	{
+		ArrayList<BlockPos> collisionBlocks = new ArrayList<>();
+		
+		EntityPlayerSP player = mc.thePlayer;
+		BlockPos pos1 =
+			new BlockPos(player.getEntityBoundingBox().minX - 0.001D,
+				player.getEntityBoundingBox().minY - 0.001D,
+				player.getEntityBoundingBox().minZ - 0.001D);
+		BlockPos pos2 =
+			new BlockPos(player.getEntityBoundingBox().maxX + 0.001D,
+				player.getEntityBoundingBox().maxY + 0.001D,
+				player.getEntityBoundingBox().maxZ + 0.001D);
+		
+		if(player.worldObj.isAreaLoaded(pos1, pos2))
+			for(int x = pos1.getX(); x <= pos2.getX(); x++)
+				for(int y = pos1.getY(); y <= pos2.getY(); y++)
+					for(int z = pos1.getZ(); z <= pos2.getZ(); z++)
+						if(y > player.posY - 1.0D && y <= player.posY)
+							collisionBlocks.add(new BlockPos(x, y, z));
+						
+		BlockPos belowPlayerPos =
+			new BlockPos(player.posX, player.posY - 1.0D, player.posZ);
+		for(BlockPos collisionBlock : collisionBlocks)
+			if(!(player.worldObj.getBlockState(collisionBlock.add(0, 1, 0))
+				.getBlock() instanceof BlockFenceGate))
+				if(player.worldObj.getBlockState(collisionBlock.add(0, 1, 0))
+					.getBlock().getCollisionBoundingBox(
+						mc.theWorld.getBlockState(collisionBlock), mc.theWorld,
+						belowPlayerPos) != null)
+					return false;
+				
+		return true;
 	}
 }
