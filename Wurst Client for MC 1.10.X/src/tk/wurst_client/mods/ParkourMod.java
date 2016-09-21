f12cc8fd10afa3d8e269ed5c871c5691ce4966c1
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-21 15:57:32 +0200

    Add Parkour
    Resolves #14

---
+++ Wurst Client for MC 1.10.X/src/tk/wurst_client/mods/ParkourMod.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.mods;
+
+import tk.wurst_client.WurstClient;
+import tk.wurst_client.events.listeners.UpdateListener;
+
+@Mod.Info(category = Mod.Category.MOVEMENT,
+	description = "Makes you jump automatically when reaching the edge of a block.\n"
+		+ "Useful for parkours, Jump'n'Runs, etc.",
+	name = "Parkour")
+public class ParkourMod extends Mod implements UpdateListener
+{
+	@Override
+	public void onEnable()
+	{
+		WurstClient.INSTANCE.events.add(UpdateListener.class, this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		if(mc.thePlayer.onGround && !mc.thePlayer.isSneaking()
+			&& mc.theWorld.getCollisionBoxes(mc.thePlayer,
+				mc.thePlayer.getEntityBoundingBox().offset(0, -0.5, 0)
+					.expand(-0.001, 0, -0.001))
+				.isEmpty())
+			mc.thePlayer.jump();
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		WurstClient.INSTANCE.events.remove(UpdateListener.class, this);
+	}
+}
