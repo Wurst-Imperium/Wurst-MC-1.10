7674eb8cd1e571487a2ea0c3c1e3f41cdc376a18
Alexander01998 <contact.wurstimperium@gmail.com>
2016-10-16 17:24:12 +0200

    Change version to 4.3

--- src/tk/wurst_client/WurstClient.java
+++ src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "4.2";
+	public static final String VERSION = "4.3";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
