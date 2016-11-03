c44e80a398df5b4163ba8e82307f80b31ce666f1
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-03 15:51:51 +0100

    Change version to 4.5

--- src/tk/wurst_client/WurstClient.java
+++ src/tk/wurst_client/WurstClient.java
@@ -26,7 +26,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "4.4";
+	public static final String VERSION = "4.5";
 	public static final String MINECRAFT_VERSION = "1.10";
 	
 	public AnalyticsManager analytics;
