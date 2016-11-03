1e6a55beb9312650775f7bfb3f9773a8634f356e
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-03 16:36:30 +0100

    Make ColorsSetting lockable

--- src/tk/wurst_client/navigator/settings/ColorsSetting.java
+++ src/tk/wurst_client/navigator/settings/ColorsSetting.java
@@ -21,6 +21,8 @@ public class ColorsSetting implements NavigatorSetting
 {
 	private String name;
 	private boolean[] selected;
+	private boolean locked;
+	private boolean[] lockSelected;
 	
 	public ColorsSetting(String name, boolean[] selected)
 	{
@@ -77,13 +79,11 @@ public class ColorsSetting implements NavigatorSetting
 		// add color buttons
 		int x = featureScreen.getMiddleX() - 104;
 		int y = 60 + featureScreen.getTextHeight() - 72;
-		String[] colorNames =
-			{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C",
-				"D", "E", "F"};
-		int[] colors =
-			new int[]{0x000000, 0x0066cc, 0x00cc00, 0x00cccc, 0xcc0000,
-				0xcc00cc, 0xff8800, 0xaaaaaa, 0x666666, 0x0000ff, 0x00ff00,
-				0x00ffff, 0xff0000, 0xff8888, 0xffff00, 0xffffff};
+		String[] colorNames = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+			"A", "B", "C", "D", "E", "F"};
+		int[] colors = new int[]{0x000000, 0x0066cc, 0x00cc00, 0x00cccc,
+			0xcc0000, 0xcc00cc, 0xff8800, 0xaaaaaa, 0x666666, 0x0000ff,
+			0x00ff00, 0x00ffff, 0xff0000, 0xff8888, 0xffff00, 0xffffff};
 		ColorButton[] buttons = new ColorButton[selected.length];
 		for(int i = 0; i < selected.length; i++)
 		{
@@ -97,9 +97,8 @@ public class ColorsSetting implements NavigatorSetting
 					x += 16;
 					break;
 			}
-			ColorButton button =
-				new ColorButton(featureScreen, x, y, colorNames[i], colors[i],
-					i);
+			ColorButton button = new ColorButton(featureScreen, x, y,
+				colorNames[i], colors[i], i);
 			buttons[i] = button;
 			featureScreen.addButton(button);
 		}
@@ -107,37 +106,37 @@ public class ColorsSetting implements NavigatorSetting
 		// all on button
 		x += 16;
 		y -= 48;
-		featureScreen.addButton(featureScreen.new ButtonData(x, y, 48, 12,
-			"All On", 0x404040)
-		{
-			@Override
-			public void press()
+		featureScreen.addButton(
+			featureScreen.new ButtonData(x, y, 48, 12, "All On", 0x404040)
 			{
-				for(int i = 0; i < buttons.length; i++)
+				@Override
+				public void press()
 				{
-					selected[i] = true;
-					buttons[i].updateColor();
+					for(int i = 0; i < buttons.length; i++)
+					{
+						selected[i] = true;
+						buttons[i].updateColor();
+					}
+					update();
 				}
-				update();
-			}
-		});
+			});
 		
 		// all off button
 		y += 16;
-		featureScreen.addButton(featureScreen.new ButtonData(x, y, 48, 12,
-			"All Off", 0x404040)
-		{
-			@Override
-			public void press()
+		featureScreen.addButton(
+			featureScreen.new ButtonData(x, y, 48, 12, "All Off", 0x404040)
 			{
-				for(int i = 0; i < buttons.length; i++)
+				@Override
+				public void press()
 				{
-					selected[i] = false;
-					buttons[i].updateColor();
+					for(int i = 0; i < buttons.length; i++)
+					{
+						selected[i] = false;
+						buttons[i].updateColor();
+					}
+					update();
 				}
-				update();
-			}
-		});
+			});
 	}
 	
 	@Override
@@ -145,16 +144,37 @@ public class ColorsSetting implements NavigatorSetting
 	{
 		return new ArrayList<>();
 	}
-
+	
 	public boolean[] getSelected()
 	{
-		return selected;
+		return locked ? lockSelected : selected;
 	}
 	
 	public void setSelected(int index, boolean selected)
 	{
-		this.selected[index] = selected;
+		if(!locked)
+		{
+			this.selected[index] = selected;
+			update();
+		}
+	}
+	
+	public final void lock(boolean[] lockSelected)
+	{
+		this.lockSelected = lockSelected;
+		locked = true;
 		update();
+	}
+	
+	public final void unlock()
+	{
+		locked = false;
+		update();
+	}
+	
+	public final boolean isLocked()
+	{
+		return locked;
 	}
 	
 	@Override
@@ -173,7 +193,7 @@ public class ColorsSetting implements NavigatorSetting
 	
 	@Override
 	public void update()
-	{	
+	{
 		
 	}
 }
