2ee7bf7c3d66dc326581ab3eccb9eb20613f7f1c
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-03 15:04:12 +0100

    Add WurstClient.enabled

--- src/tk/wurst_client/WurstClient.java
+++ src/tk/wurst_client/WurstClient.java
@@ -42,6 +42,8 @@ public enum WurstClient
 	public SpfManager special;
 	public Updater updater;
 	
+	private boolean enabled = true;
+	
 	public void startClient()
 	{
 		events = new EventManager();
@@ -65,5 +67,15 @@ public enum WurstClient
 		files.saveOptions();
 		
 		FrameHook.maximize();
+	}
+
+	public boolean isEnabled()
+	{
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled)
+	{
+		this.enabled = enabled;
 	}
 }
