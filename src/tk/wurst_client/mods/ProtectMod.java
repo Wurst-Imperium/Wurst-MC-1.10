d53a0aaf868496cf6457c3737c82ba3ff78cc8f7
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-05 17:04:41 +0100

    Update ProtectMod

--- src/tk/wurst_client/mods/ProtectMod.java
+++ src/tk/wurst_client/mods/ProtectMod.java
@@ -7,25 +7,115 @@
  */
 package tk.wurst_client.mods;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumHand;
 import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Bypasses;
 import tk.wurst_client.mods.Mod.Info;
 import tk.wurst_client.utils.EntityUtils;
+import tk.wurst_client.utils.EntityUtils.TargetSettings;
 
 @Info(
-	description = "A bot that follows the closest entity and protects it.",
+	description = "A bot that follows the closest entity and protects it from other entities.\n"
+		+ "Use .protect <entity> to protect a specific entity instead of the closest one.",
 	name = "Protect",
 	help = "Mods/Protect")
 @Bypasses(ghostMode = false)
 public class ProtectMod extends Mod implements UpdateListener
 {
-	private EntityLivingBase friend;
-	private EntityLivingBase enemy;
+	private Entity friend;
+	private Entity enemy;
 	private float range = 6F;
 	private double distanceF = 2D;
 	private double distanceE = 3D;
+	
+	private TargetSettings friendSettingsFind = new TargetSettings()
+	{
+		@Override
+		public boolean targetFriends()
+		{
+			return true;
+		}
+		
+		@Override
+		public float getRange()
+		{
+			return range;
+		}
+	};
+	
+	private TargetSettings friendSettingsKeep = new TargetSettings()
+	{
+		@Override
+		public boolean targetFriends()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetBehindWalls()
+		{
+			return true;
+		};
+		
+		@Override
+		public boolean targetPlayers()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetAnimals()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetMonsters()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetGolems()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetSleepingPlayers()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetInvisiblePlayers()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetInvisibleMobs()
+		{
+			return true;
+		}
+		
+		@Override
+		public boolean targetTeams()
+		{
+			return false;
+		}
+	};
+	
+	private TargetSettings enemySettings = new TargetSettings()
+	{
+		@Override
+		public float getRange()
+		{
+			return range;
+		};
+	};
 	
 	@Override
 	public String getRenderName()
@@ -39,62 +129,75 @@ public class ProtectMod extends Mod implements UpdateListener
 	@Override
 	public void onEnable()
 	{
-		friend = null;
-		EntityLivingBase en = EntityUtils.getClosestEntity(false, 360, false);
-		if(en != null && mc.thePlayer.getDistanceToEntity(en) <= range)
-			friend = en;
+		// set friend
+		friend = EntityUtils.getClosestEntity(friendSettingsFind);
+		
 		wurst.events.add(UpdateListener.class, this);
 	}
 	
 	@Override
 	public void onUpdate()
 	{
-		if(friend == null || friend.isDead || friend.getHealth() <= 0
-			|| mc.thePlayer.getHealth() <= 0)
+		// check if player died, friend died or friend disappeared
+		if(mc.thePlayer.getHealth() <= 0
+			|| !EntityUtils.isCorrectEntity(friend, friendSettingsKeep))
 		{
 			friend = null;
 			enemy = null;
 			setEnabled(false);
 			return;
 		}
-		if(enemy != null && (enemy.getHealth() <= 0 || enemy.isDead))
-			enemy = null;
-		double xDistF = Math.abs(mc.thePlayer.posX - friend.posX);
-		double zDistF = Math.abs(mc.thePlayer.posZ - friend.posZ);
-		double xDistE = distanceE;
-		double zDistE = distanceE;
-		if(enemy != null && mc.thePlayer.getDistanceToEntity(enemy) <= range)
-		{
-			xDistE = Math.abs(mc.thePlayer.posX - enemy.posX);
-			zDistE = Math.abs(mc.thePlayer.posZ - enemy.posZ);
-		}else
-			EntityUtils.faceEntityClient(friend);
-		if((xDistF > distanceF || zDistF > distanceF)
-			&& (enemy == null || mc.thePlayer.getDistanceToEntity(enemy) > range)
-			|| xDistE > distanceE || zDistE > distanceE)
-			mc.gameSettings.keyBindForward.pressed = true;
-		else
-			mc.gameSettings.keyBindForward.pressed = false;
+		
+		// set enemy
+		enemy = EntityUtils.getClosestEntityOtherThan(friend, enemySettings);
+		
+		// jump if necessary
 		if(mc.thePlayer.isCollidedHorizontally && mc.thePlayer.onGround)
 			mc.thePlayer.jump();
-		if(mc.thePlayer.isInWater() && mc.thePlayer.posY < friend.posY)
+		
+		// swim up if necessary
+		if(mc.thePlayer.isInWater()
+			&& mc.thePlayer.posY < (enemy != null ? enemy.posY : friend.posY))
 			mc.thePlayer.motionY += 0.04;
+		
+		// update timer
 		updateMS();
-		if(hasTimePassedS(wurst.mods.killauraMod.speed.getValueF())
-			&& EntityUtils.getClosestEnemy(friend) != null)
+		
+		if(enemy == null)
 		{
-			enemy = EntityUtils.getClosestEnemy(friend);
-			if(mc.thePlayer.getDistanceToEntity(enemy) <= range)
-			{
-				if(wurst.mods.autoSwordMod.isActive())
-					AutoSwordMod.setSlot();
-				wurst.mods.criticalsMod.doCritical();
-				wurst.mods.blockHitMod.doBlock();
-				EntityUtils.faceEntityClient(enemy);
-				mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
-				mc.playerController.attackEntity(mc.thePlayer, enemy);
-				updateLastMS();
-			}
+			// follow friend
+			EntityUtils.faceEntityClient(friend);
+			mc.gameSettings.keyBindForward.pressed =
+				mc.thePlayer.getDistanceToEntity(friend) > distanceF;
+		}else
+		{
+			// follow enemy
+			EntityUtils.faceEntityClient(enemy);
+			mc.gameSettings.keyBindForward.pressed =
+				mc.thePlayer.getDistanceToEntity(enemy) > distanceE;
+			
+			// check timer / cooldown
+			if(wurst.mods.killauraMod.useCooldown.isChecked()
+				? mc.thePlayer.getCooledAttackStrength(0F) < 1F
+				: !hasTimePassedS(wurst.mods.killauraMod.speed.getValueF()))
+				return;
+			
+			// AutoSword
+			if(wurst.mods.autoSwordMod.isActive())
+				AutoSwordMod.setSlot();
+			
+			// Criticals
+			wurst.mods.criticalsMod.doCritical();
+			
+			// BlockHit
+			wurst.mods.blockHitMod.doBlock();
+			
+			// attack enemy
+			mc.playerController.attackEntity(mc.thePlayer, enemy);
+			mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
+			
+			// reset timer
+			updateLastMS();
 		}
 	}
 	
