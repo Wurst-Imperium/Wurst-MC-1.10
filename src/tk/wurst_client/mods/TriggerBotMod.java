bad21f98decd8b812ff2f046b98beb2a13154219
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-09 10:38:05 +0100

    Update TriggerBotMod

--- src/tk/wurst_client/mods/TriggerBotMod.java
+++ src/tk/wurst_client/mods/TriggerBotMod.java
@@ -7,9 +7,7 @@
  */
 package tk.wurst_client.mods;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.RayTraceResult.Type;
 import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Bypasses;
 import tk.wurst_client.mods.Mod.Info;
@@ -19,48 +17,57 @@ import tk.wurst_client.navigator.settings.SliderSetting;
 import tk.wurst_client.navigator.settings.SliderSetting.ValueDisplay;
 import tk.wurst_client.special.YesCheatSpf.BypassLevel;
 import tk.wurst_client.utils.EntityUtils;
+import tk.wurst_client.utils.EntityUtils.TargetSettings;
 
-@Info(
-	description = "Automatically attacks the entity you're looking at.",
+@Info(description = "Automatically attacks the entity you're looking at.",
 	name = "TriggerBot",
 	tags = "trigger bot",
 	help = "Mods/TriggerBot")
 @Bypasses
 public class TriggerBotMod extends Mod implements UpdateListener
 {
-	public CheckboxSetting useKillaura = new CheckboxSetting(
-		"Use Killaura settings", true)
+	public CheckboxSetting useKillaura =
+		new CheckboxSetting("Use Killaura settings", true)
+		{
+			@Override
+			public void update()
+			{
+				if(isChecked())
+				{
+					KillauraMod killaura = wurst.mods.killauraMod;
+					useCooldown.lock(killaura.useCooldown.isChecked());
+					speed.lockToValue(killaura.speed.getValue());
+					range.lockToValue(killaura.range.getValue());
+				}else
+				{
+					useCooldown.unlock();
+					speed.unlock();
+					range.unlock();
+				}
+			};
+		};
+	public CheckboxSetting useCooldown =
+		new CheckboxSetting("Use Attack Cooldown as Speed", true)
+		{
+			@Override
+			public void update()
+			{
+				speed.setDisabled(isChecked());
+			};
+		};
+	public SliderSetting speed =
+		new SliderSetting("Speed", 20, 2, 20, 0.1, ValueDisplay.DECIMAL);
+	public SliderSetting range =
+		new SliderSetting("Range", 6, 1, 6, 0.05, ValueDisplay.DECIMAL);
+	
+	private TargetSettings targetSettings = new TargetSettings()
 	{
 		@Override
-		public void update()
+		public float getRange()
 		{
-			if(isChecked())
-			{
-				KillauraMod killaura = wurst.mods.killauraMod;
-				useCooldown.lock(killaura.useCooldown.isChecked());
-				speed.lockToValue(killaura.speed.getValue());
-				range.lockToValue(killaura.range.getValue());
-			}else
-			{
-				useCooldown.unlock();
-				speed.unlock();
-				range.unlock();
-			}
-		};
+			return range.getValueF();
+		}
 	};
-	public CheckboxSetting useCooldown = new CheckboxSetting(
-		"Use Attack Cooldown as Speed", true)
-	{
-		@Override
-		public void update()
-		{
-			speed.setDisabled(isChecked());
-		};
-	};
-	public SliderSetting speed = new SliderSetting("Speed", 20, 2, 20, 0.1,
-		ValueDisplay.DECIMAL);
-	public SliderSetting range = new SliderSetting("Range", 6, 1, 6, 0.05,
-		ValueDisplay.DECIMAL);
 	
 	@Override
 	public void initSettings()
@@ -100,29 +107,37 @@ public class TriggerBotMod extends Mod implements UpdateListener
 	@Override
 	public void onUpdate()
 	{
+		// update timer
 		updateMS();
 		
-		if(mc.objectMouseOver == null
-			|| mc.objectMouseOver.typeOfHit != Type.ENTITY
-			|| !(mc.objectMouseOver.entityHit instanceof EntityLivingBase))
+		// check timer / cooldown
+		if(useCooldown.isChecked()
+			? mc.thePlayer.getCooledAttackStrength(0F) < 1F
+			: !hasTimePassedS(speed.getValueF()))
 			return;
-		EntityLivingBase en = (EntityLivingBase)mc.objectMouseOver.entityHit;
 		
-		if((useCooldown.isChecked() ? mc.thePlayer.getCooledAttackStrength(0F) >= 1F
-			: hasTimePassedS(speed.getValueF()))
-			&& mc.thePlayer.getDistanceToEntity(en) <= range.getValueF()
-			&& EntityUtils.isCorrectEntity(en, true))
-		{
-			if(wurst.mods.autoSwordMod.isActive())
-				AutoSwordMod.setSlot();
-			wurst.mods.criticalsMod.doCritical();
-			wurst.mods.blockHitMod.doBlock();
-			
-			mc.playerController.attackEntity(mc.thePlayer, en);
-			mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
-			
-			updateLastMS();
-		}
+		// check entity
+		if(mc.objectMouseOver == null || !EntityUtils
+			.isCorrectEntity(mc.objectMouseOver.entityHit, targetSettings))
+			return;
+		
+		// AutoSword
+		if(wurst.mods.autoSwordMod.isActive())
+			AutoSwordMod.setSlot();
+		
+		// Criticals
+		wurst.mods.criticalsMod.doCritical();
+		
+		// BlockHit
+		wurst.mods.blockHitMod.doBlock();
+		
+		// attack entity
+		mc.playerController.attackEntity(mc.thePlayer,
+			mc.objectMouseOver.entityHit);
+		mc.thePlayer.swingArm(EnumHand.MAIN_HAND);
+		
+		// reset timer
+		updateLastMS();
 	}
 	
 	@Override
