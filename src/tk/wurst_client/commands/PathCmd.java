227ec618c15c7749b52a632d13550c840715262b
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-10 15:11:38 +0100

    :art: Redesign .path

--- src/tk/wurst_client/commands/PathCmd.java
+++ src/tk/wurst_client/commands/PathCmd.java
@@ -7,12 +7,16 @@
  */
 package tk.wurst_client.commands;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 import tk.wurst_client.ai.PathFinder;
 import tk.wurst_client.ai.PathPoint;
 import tk.wurst_client.commands.Cmd.Info;
 import tk.wurst_client.events.listeners.RenderListener;
-import tk.wurst_client.utils.RenderUtils;
 import tk.wurst_client.utils.EntityUtils.TargetSettings;
 
 @Info(
@@ -79,11 +83,93 @@ public class PathCmd extends Cmd implements RenderListener
 	@Override
 	public void onRender()
 	{
-		PathPoint path2 = path;
-		while(path2 != null)
+		PathPoint currentPoint = path;
+		while(currentPoint != null && currentPoint.getPrevious() != null)
 		{
-			RenderUtils.blockEsp(path2.getPos());
-			path2 = path2.getPrevious();
+			PathPoint prevPoint = currentPoint.getPrevious();
+			
+			double x = currentPoint.getPos().getX() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosX;
+			double y = currentPoint.getPos().getY() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosY;
+			double z = currentPoint.getPos().getZ() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosZ;
+			
+			double prevX = prevPoint.getPos().getX() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosX;
+			double prevY = prevPoint.getPos().getY() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosY;
+			double prevZ = prevPoint.getPos().getZ() + 0.5
+				- Minecraft.getMinecraft().getRenderManager().renderPosZ;
+			
+			glBlendFunc(770, 771);
+			glEnable(GL_BLEND);
+			glEnable(GL_LINE_SMOOTH);
+			glLineWidth(2.0F);
+			glDisable(GL11.GL_TEXTURE_2D);
+			glDisable(GL_DEPTH_TEST);
+			glDisable(GL_CULL_FACE);
+			glDepthMask(false);
+			
+			glColor4f(0F, 1F, 0F, 0.75F);
+			glBegin(GL_LINES);
+			{
+				glVertex3d(prevX, prevY, prevZ);
+				glVertex3d(x, y, z);
+			}
+			glEnd();
+			
+			glPushMatrix();
+			glTranslated(x, y, z);
+			glScaled(1D / 16D, 1D / 16D, 1D / 16D);
+			glRotated(Math.toDegrees(Math.atan2(y - prevY, prevZ - z)) + 90, 1,
+				0, 0);
+			glRotated(
+				Math.toDegrees(Math.atan2(x - prevX,
+					Math.sqrt(
+						Math.pow(prevY - y, 2) + Math.pow(prevZ - z, 2)))),
+				0, 0, 1);
+			glBegin(GL_LINES);
+			{
+				glVertex3d(0, 2, 1);
+				glVertex3d(-1, 2, 0);
+				
+				glVertex3d(-1, 2, 0);
+				glVertex3d(0, 2, -1);
+				
+				glVertex3d(0, 2, -1);
+				glVertex3d(1, 2, 0);
+				
+				glVertex3d(1, 2, 0);
+				glVertex3d(0, 2, 1);
+				
+				glVertex3d(1, 2, 0);
+				glVertex3d(-1, 2, 0);
+				
+				glVertex3d(0, 2, 1);
+				glVertex3d(0, 2, -1);
+				
+				glVertex3d(0, 0, 0);
+				glVertex3d(1, 2, 0);
+				
+				glVertex3d(0, 0, 0);
+				glVertex3d(-1, 2, 0);
+				
+				glVertex3d(0, 0, 0);
+				glVertex3d(0, 2, -1);
+				
+				glVertex3d(0, 0, 0);
+				glVertex3d(0, 2, 1);
+			}
+			glEnd();
+			glPopMatrix();
+			
+			glEnable(GL11.GL_TEXTURE_2D);
+			glEnable(GL_DEPTH_TEST);
+			glDepthMask(true);
+			glDisable(GL_BLEND);
+			
+			currentPoint = prevPoint;
 		}
 	}
 }
