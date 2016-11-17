567f96cddf62451f5f6a21fc97ea972fa6718243
Alexander01998 <contact.wurstimperium@gmail.com>
2016-11-17 11:22:34 +0100

    Remove thread in PathCmd

--- src/tk/wurst_client/commands/PathCmd.java
+++ src/tk/wurst_client/commands/PathCmd.java
@@ -19,6 +19,7 @@ import tk.wurst_client.ai.PathFinder;
 import tk.wurst_client.ai.PathPoint;
 import tk.wurst_client.commands.Cmd.Info;
 import tk.wurst_client.events.listeners.RenderListener;
+import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.navigator.settings.CheckboxSetting;
 import tk.wurst_client.utils.EntityUtils.TargetSettings;
 
@@ -27,12 +28,12 @@ import tk.wurst_client.utils.EntityUtils.TargetSettings;
 	name = "path",
 	syntax = {"<x> <y> <z>", "<entity>", "-debug", "-depth", "-refresh"},
 	help = "Commands/path")
-public class PathCmd extends Cmd implements RenderListener
+public class PathCmd extends Cmd implements UpdateListener, RenderListener
 {
 	private PathFinder pathFinder;
 	private ArrayList<BlockPos> path;
 	private boolean enabled;
-	private BlockPos lastPos;
+	private BlockPos lastGoal;
 	
 	private TargetSettings targetSettings = new TargetSettings()
 	{
@@ -51,6 +52,7 @@ public class PathCmd extends Cmd implements RenderListener
 	
 	public CheckboxSetting debugMode = new CheckboxSetting("Debug mode", false);
 	public CheckboxSetting depthTest = new CheckboxSetting("Depth test", false);
+	private long startTime;
 	
 	public PathCmd()
 	{
@@ -61,6 +63,7 @@ public class PathCmd extends Cmd implements RenderListener
 	@Override
 	public void execute(String[] args) throws Error
 	{
+		// process special commands
 		boolean refresh = false;
 		if(args.length > 0 && args[0].startsWith("-"))
 			switch(args[0])
@@ -76,16 +79,16 @@ public class PathCmd extends Cmd implements RenderListener
 						+ (depthTest.isChecked() ? "on" : "off") + ".");
 					return;
 				case "-refresh":
-					if(lastPos == null)
+					if(lastGoal == null)
 						error("Cannot refresh: no previous path.");
 					refresh = true;
 					break;
 			}
 		
-		path = new ArrayList<>();
-		
+		// disable if enabled
 		if(enabled)
 		{
+			wurst.events.remove(UpdateListener.class, this);
 			wurst.events.remove(RenderListener.class, this);
 			enabled = false;
 			
@@ -93,44 +96,52 @@ public class PathCmd extends Cmd implements RenderListener
 				return;
 		}
 		
-		final BlockPos pos;
+		// set PathFinder
+		final BlockPos goal;
 		if(refresh)
-			pos = lastPos;
+			goal = lastGoal;
 		else
 		{
 			int[] posArray = argsToPos(targetSettings, args);
-			pos = new BlockPos(posArray[0], posArray[1], posArray[2]);
-			lastPos = pos;
+			goal = new BlockPos(posArray[0], posArray[1], posArray[2]);
+			lastGoal = goal;
 		}
+		pathFinder = new PathFinder(goal);
 		
-		Thread thread = new Thread(new Runnable()
+		// clear path
+		path = new ArrayList<>();
+		
+		// start
+		enabled = true;
+		wurst.events.add(UpdateListener.class, this);
+		System.out.println("Finding path...");
+		startTime = System.currentTimeMillis();
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		long passedTime = System.currentTimeMillis() - startTime;
+		boolean foundPath = pathFinder.process(1024);
+		
+		// stop if path if found or 10s have passed
+		if(foundPath || passedTime >= 10e3)
 		{
-			@Override
-			public void run()
-			{
-				System.out.println("Finding path...");
-				long startTime = System.nanoTime();
-				pathFinder = new PathFinder(pos);
-				if(pathFinder.find())
-				{
-					path = pathFinder.formatPath();
-				}else
-					wurst.chat.error("Could not find a path.");
-				enabled = true;
-				wurst.events.add(RenderListener.class, PathCmd.this);
-				
-				if(debugMode.isChecked())
-					System.out.println("Length: " + path.size()
-						+ ", processed: "
-						+ pathFinder.getProcessedPoints().size() + ", queue: "
-						+ pathFinder.getQueuedPoints().length + ", cost: "
-						+ pathFinder.getCurrentPoint().getTotalCost());
-				System.out.println("Done after "
-					+ (System.nanoTime() - startTime) / 1e6 + "ms");
-			}
-		});
-		thread.setPriority(Thread.MIN_PRIORITY);
-		thread.start();
+			if(foundPath)
+				path = pathFinder.formatPath();
+			else
+				wurst.chat.error("Could not find a path.");
+			
+			wurst.events.remove(UpdateListener.class, this);
+			wurst.events.add(RenderListener.class, this);
+			
+			System.out.println("Done after " + (passedTime) + "ms");
+			if(debugMode.isChecked())
+				System.out.println("Length: " + path.size() + ", processed: "
+					+ pathFinder.getProcessedPoints().size() + ", queue: "
+					+ pathFinder.getQueuedPoints().length + ", cost: "
+					+ pathFinder.getCurrentPoint().getTotalCost());
+		}
 	}
 	
 	@Override
