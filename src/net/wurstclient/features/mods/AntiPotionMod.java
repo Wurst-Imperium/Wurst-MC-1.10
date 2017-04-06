75734fb70805f740ba8039a15b88d9164404c22f
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-06 19:53:50 +0200

    Update AntiPotionMod

--- src/net/wurstclient/features/mods/AntiPotionMod.java
+++ src/net/wurstclient/features/mods/AntiPotionMod.java
@@ -7,9 +7,9 @@
  */
 package net.wurstclient.features.mods;
 
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.init.MobEffects;
 import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.potion.Potion;
 import net.wurstclient.compatibility.WConnection;
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.UpdateListener;
@@ -21,6 +21,11 @@ import net.wurstclient.events.listeners.UpdateListener;
 @Mod.Bypasses(ghostMode = false, latestNCP = false, olderNCP = false)
 public final class AntiPotionMod extends Mod implements UpdateListener
 {
+	private final Potion[] blockedEffects = new Potion[]{MobEffects.HUNGER,
+		MobEffects.SLOWNESS, MobEffects.MINING_FATIGUE,
+		MobEffects.INSTANT_DAMAGE, MobEffects.NAUSEA, MobEffects.BLINDNESS,
+		MobEffects.WEAKNESS, MobEffects.WITHER, MobEffects.POISON};
+	
 	@Override
 	public void onEnable()
 	{
@@ -28,27 +33,40 @@ public final class AntiPotionMod extends Mod implements UpdateListener
 	}
 	
 	@Override
-	public void onUpdate()
-	{
-		EntityPlayerSP player = WMinecraft.getPlayer();
-		if(!player.capabilities.isCreativeMode && player.onGround
-			&& !player.getActivePotionEffects().isEmpty())
-			if(player.isPotionActive(MobEffects.HUNGER)
-				|| player.isPotionActive(MobEffects.SLOWNESS)
-				|| player.isPotionActive(MobEffects.MINING_FATIGUE)
-				|| player.isPotionActive(MobEffects.INSTANT_DAMAGE)
-				|| player.isPotionActive(MobEffects.NAUSEA)
-				|| player.isPotionActive(MobEffects.BLINDNESS)
-				|| player.isPotionActive(MobEffects.WEAKNESS)
-				|| player.isPotionActive(MobEffects.WITHER)
-				|| player.isPotionActive(MobEffects.POISON))
-				for(int i = 0; i < 1000; i++)
-					WConnection.sendPacket(new CPacketPlayer());
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		// check gamemode
+		if(WMinecraft.getPlayer().capabilities.isCreativeMode)
+			return;
+		
+		// check onGround
+		if(!WMinecraft.getPlayer().onGround)
+			return;
+		
+		// check effects
+		if(!hasBadEffect())
+			return;
+		
+		// send packets
+		for(int i = 0; i < 1000; i++)
+			WConnection.sendPacket(new CPacketPlayer());
+	}
+	
+	private boolean hasBadEffect()
+	{
+		if(WMinecraft.getPlayer().getActivePotionEffects().isEmpty())
+			return false;
+		
+		for(Potion effect : blockedEffects)
+			if(WMinecraft.getPlayer().isPotionActive(effect))
+				return true;
+			
+		return false;
 	}
 }
