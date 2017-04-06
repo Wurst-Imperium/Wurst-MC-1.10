c878a2e46627a69c533b7916ff8010d6d600a8a8
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-06 19:56:42 +0200

    Update AutoArmorMod

--- src/net/wurstclient/features/mods/AutoArmorMod.java
+++ src/net/wurstclient/features/mods/AutoArmorMod.java
@@ -8,91 +8,118 @@
 package net.wurstclient.features.mods;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.UpdateListener;
+import net.wurstclient.utils.InventoryUtils;
 
 @Mod.Info(description = "Manages your armor automatically.",
 	name = "AutoArmor",
 	tags = "auto armor",
 	help = "Mods/AutoArmor")
-@Mod.Bypasses(ghostMode = false, latestNCP = false)
+@Mod.Bypasses
 public final class AutoArmorMod extends Mod implements UpdateListener
 {
-	private int[] bestArmor;
+	private int timer;
 	
 	@Override
 	public void onEnable()
 	{
+		// reset timer
+		timer = 0;
+		
 		wurst.events.add(UpdateListener.class, this);
-	}
-	
-	@Override
-	public void onUpdate()
-	{
-		if(WMinecraft.getPlayer().capabilities.isCreativeMode
-			|| mc.currentScreen instanceof GuiContainer
-				&& !(mc.currentScreen instanceof GuiInventory))
-			return;
-		updateMS();
-		if(hasTimePassedM(3000))
-		{
-			bestArmor = new int[]{-1, -1, -1, -1};
-			for(int i = 0; i < 36; i++)
-			{
-				ItemStack itemstack =
-					WMinecraft.getPlayer().inventory.getStackInSlot(i);
-				if(itemstack != null
-					&& itemstack.getItem() instanceof ItemArmor)
-				{
-					ItemArmor armor = (ItemArmor)itemstack.getItem();
-					if(armor.damageReduceAmount > bestArmor[armor.armorType
-						.ordinal() - 2])
-						bestArmor[armor.armorType.ordinal() - 2] = i;
-				}
-			}
-			for(int i = 0; i < 4; i++)
-			{
-				ItemStack itemstack =
-					WMinecraft.getPlayer().inventory.armorItemInSlot(i);
-				ItemArmor currentArmor;
-				if(itemstack != null
-					&& itemstack.getItem() instanceof ItemArmor)
-					currentArmor = (ItemArmor)itemstack.getItem();
-				else
-					currentArmor = null;
-				ItemArmor bestArmor;
-				try
-				{
-					bestArmor = (ItemArmor)WMinecraft.getPlayer().inventory
-						.getStackInSlot(this.bestArmor[i]).getItem();
-				}catch(Exception e)
-				{
-					bestArmor = null;
-				}
-				if(bestArmor != null && (currentArmor == null
-					|| bestArmor.damageReduceAmount > currentArmor.damageReduceAmount))
-					if(WMinecraft.getPlayer().inventory
-						.getFirstEmptyStack() != -1 || currentArmor == null)
-					{
-						mc.playerController.windowClick(0, 8 - i, 0,
-							ClickType.QUICK_MOVE, WMinecraft.getPlayer());
-						mc.playerController.windowClick(0,
-							this.bestArmor[i] < 9 ? 36 + this.bestArmor[i]
-								: this.bestArmor[i],
-							0, ClickType.QUICK_MOVE, WMinecraft.getPlayer());
-					}
-			}
-			updateLastMS();
-		}
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		// wait for timer
+		if(timer > 0)
+		{
+			timer--;
+			return;
+		}
+		
+		// check screen
+		if(mc.currentScreen instanceof GuiContainer
+			&& !(mc.currentScreen instanceof InventoryEffectRenderer))
+			return;
+		
+		// store slots and values of best armor pieces
+		int[] bestArmorSlots = new int[4];
+		int[] bestArmorValues = new int[4];
+		
+		// initialize with currently equipped armor
+		for(int armorType = 0; armorType < 4; armorType++)
+		{
+			ItemStack oldArmor =
+				WMinecraft.getPlayer().inventory.armorItemInSlot(armorType);
+			if(oldArmor != null && oldArmor.getItem() instanceof ItemArmor)
+				bestArmorValues[armorType] =
+					((ItemArmor)oldArmor.getItem()).damageReduceAmount;
+			
+			bestArmorSlots[armorType] = -1;
+		}
+		
+		// search inventory for better armor
+		for(int slot = 0; slot < 36; slot++)
+		{
+			ItemStack stack =
+				WMinecraft.getPlayer().inventory.getStackInSlot(slot);
+			if(stack == null || !(stack.getItem() instanceof ItemArmor))
+				continue;
+			
+			ItemArmor armor = (ItemArmor)stack.getItem();
+			int armorType = InventoryUtils.getArmorType(armor);
+			int armorValue = armor.damageReduceAmount;
+			
+			if(armorValue > bestArmorValues[armorType])
+			{
+				bestArmorSlots[armorType] = slot;
+				bestArmorValues[armorType] = armorValue;
+			}
+		}
+		
+		// equip better armor
+		for(int armorType = 0; armorType < 4; armorType++)
+		{
+			// check if better armor was found
+			int slot = bestArmorSlots[armorType];
+			if(slot == -1)
+				continue;
+				
+			// check if armor can be swapped
+			// needs 1 free slot where it can put the old armor
+			ItemStack oldArmor =
+				WMinecraft.getPlayer().inventory.armorItemInSlot(armorType);
+			if(oldArmor == null || !InventoryUtils.isEmptySlot(oldArmor)
+				|| WMinecraft.getPlayer().inventory.getFirstEmptyStack() != -1)
+			{
+				// hotbar fix
+				if(slot < 9)
+					slot += 36;
+				
+				// swap armor
+				mc.playerController.windowClick(0, 8 - armorType, 0,
+					ClickType.QUICK_MOVE, WMinecraft.getPlayer());
+				mc.playerController.windowClick(0, slot, 0,
+					ClickType.QUICK_MOVE, WMinecraft.getPlayer());
+				
+				break;
+			}
+		}
+		
+		// set timer
+		timer = 2;
 	}
 }
