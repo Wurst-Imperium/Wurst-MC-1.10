f1887e3a4682ee5d0774b2ca1f8ea2dfd1fd06eb
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-22 12:00:51 +0200

    Add WMinecraft.DISPLAY_VERSION

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -8,6 +8,7 @@ import net.minecraft.client.network.NetHandlerPlayClient;
 public final class WMinecraft
 {
 	public static final String VERSION = "1.10";
+	public static final String DISPLAY_VERSION = "1.10";
 	public static final boolean REALMS = false;
 	public static final boolean COOLDOWN = true;
 	
