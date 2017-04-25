0cd3d45040fb2b6195d64a0b4c7256933fa834e0
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-25 10:22:33 +0200

    Move Cmd, Mod & Spf to net.wurstclient.features

--- src/net/wurstclient/features/mods/FreecamMod.java
+++ src/net/wurstclient/features/mods/FreecamMod.java
@@ -10,6 +10,7 @@ package net.wurstclient.features.mods;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.UpdateListener;
+import net.wurstclient.features.Mod;
 
 @Mod.Info(tags = "free cam, spectator", help = "Mods/Freecam")
 @Mod.Bypasses
