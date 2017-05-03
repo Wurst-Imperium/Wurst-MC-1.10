2efbed3382952fc5815d1b72db0abe04f1a29a7e
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-03 20:58:51 +0200

    Add WMinecraft.OPTIFINE

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -13,6 +13,7 @@ public final class WMinecraft
 {
 	public static final String VERSION = "1.10";
 	public static final String DISPLAY_VERSION = "1.10";
+	public static final boolean OPTIFINE = false;
 	public static final boolean REALMS = false;
 	public static final boolean COOLDOWN = true;
 	
